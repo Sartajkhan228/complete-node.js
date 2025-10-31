e30f570b5f7aa1d81b82d88a06bef605e9dba4dc
Sartajkhan228 <khanalam7328@gmail.com>
2025-10-31 13:41:48 +0500

    modules in node

---
+++ math1.js
@@ -0,0 +1,16 @@
+
+export function subtract(a, b) {
+    return a - b;
+}
+
+export function multiply(a, b) {
+    return a * b;
+}
+
+export function divide(a, b) {
+    return a / b;
+}
+
+export const PI = 3.14159;
+
+export default { subtract, multiply, divide, PI };
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import add from './math.js';
+import { subtract, multiply, divide, PI } from './math1.js';
 
 
 const app = express();
@@ -8,7 +9,11 @@ app.get('/', (req, res) => {
     res.send('Hello World!');
 })
 
-console.log(add(2, 4))
+console.log(add(2, 4));
+console.log(subtract(2, 4));
+console.log(multiply(2, 4));
+console.log(divide(2, 4));
+console.log(PI);
 
 const PORT = process.env.PORT || 3000;
 
